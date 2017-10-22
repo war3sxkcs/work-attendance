b93c83f82b4bcb2df25209ddc0f8db39e34c47fb
醉美小坤坤 <songxiankun2008@126.com>
2017-10-22 17:02:18 +0800

    生成attend实体

--- src/main/java/com/smallkk/attend/controller/AttendController.java
+++ src/main/java/com/smallkk/attend/controller/AttendController.java
@@ -1,26 +1,22 @@
 package com.smallkk.attend.controller;
-
 import com.smallkk.attend.entity.Attend;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 /**
  * Created By  醉美柳舞之众星捧月
  *
  * @author song
- * @date 2017/10/22 16:17
+ * @date 2017/10/22 16:52
  */
 @Controller
 @RequestMapping("attend")
 public class AttendController {
-    @RequestMapping()
-    public String attend() {
+    @RequestMapping
+    public String toAttend() {
         return "attend";
     }
-
-    @RequestMapping()
-    public String sign(@RequestBody Attend attend) {
-        return "";
+    public String signAttend(@RequestBody Attend attend) {
+        return null;
     }
 }
--- src/main/java/com/smallkk/attend/service/AttendService.java
+++ src/main/java/com/smallkk/attend/service/AttendService.java
@@ -1,20 +1,13 @@
 package com.smallkk.attend.service;
 
 import com.smallkk.attend.entity.Attend;
-import org.springframework.stereotype.Service;
 
 /**
  * Created By  醉美柳舞之众星捧月
  *
  * @author song
- * @date 2017/10/22 16:30
+ * @date 2017/10/22 16:54
  */
 public interface AttendService {
-    /**
-     * Author song  醉美柳舞之众星捧月
-     * Date & Time  2017/10/22 16:31
-     * Description  签到
-     */
-
     void signAttend(Attend attend);
 }
